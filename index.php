f015adb4d4a31a13ba896c1c7121431164b62bb9
Dieudonne GUSENGA <dieudonne.gusenga@rura.rw>
2025-04-25 22:53:01 +0200

    Added

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Dieudonne PHP</title>
+</head>
+<body>
+    <h1>Dieudonne PHP</h1>
+    <pre style="margin-left: 4em;">
+        Y Y Y Y Y
+        Y Y   Y Y
+        Y Y
+        Y Y
+        Y Y
+        Y Y
+        Y Y
+    </pre>
+    <?php
+        print hash('sha256', 'Dieudonne');
+    ?>
+</body>
+</html>
